0a081cdd4eb23be0ba93c269b8a9b49442e2ff65
Krasi Georgiev <8903888+krasi-georgiev@users.noreply.github.com>
2021-11-29 13:03:34 +0200

    install  bingo
    Signed-off-by: Krasi Georgiev <8903888+krasi-georgiev@users.noreply.github.com>

---
+++ .bingo/Variables.mk
@@ -0,0 +1,43 @@
+# Auto generated binary variables helper managed by https://github.com/bwplotka/bingo v0.4.3. DO NOT EDIT.
+# All tools are designed to be build inside $GOBIN.
+BINGO_DIR := $(dir $(lastword $(MAKEFILE_LIST)))
+GOPATH ?= $(shell go env GOPATH)
+GOBIN  ?= $(firstword $(subst :, ,${GOPATH}))/bin
+GO     ?= $(shell which go)
+
+# Below generated variables ensure that every time a tool under each variable is invoked, the correct version
+# will be used; reinstalling only if needed.
+# For example for faillint variable:
+#
+# In your main Makefile (for non array binaries):
+#
+#include .bingo/Variables.mk # Assuming -dir was set to .bingo .
+#
+#command: $(FAILLINT)
+#	@echo "Running faillint"
+#	@$(FAILLINT) <flags/args..>
+#
+FAILLINT := $(GOBIN)/faillint-v1.7.0
+$(FAILLINT): $(BINGO_DIR)/faillint.mod
+	@# Install binary/ries using Go 1.14+ build command. This is using bwplotka/bingo-controlled, separate go module with pinned dependencies.
+	@echo "(re)installing $(GOBIN)/faillint-v1.7.0"
+	@cd $(BINGO_DIR) && $(GO) build -mod=mod -modfile=faillint.mod -o=$(GOBIN)/faillint-v1.7.0 "github.com/fatih/faillint"
+
+GOIMPORTS := $(GOBIN)/goimports-v0.0.0-20200925163029-4c77dbd9e533
+$(GOIMPORTS): $(BINGO_DIR)/goimports.mod
+	@# Install binary/ries using Go 1.14+ build command. This is using bwplotka/bingo-controlled, separate go module with pinned dependencies.
+	@echo "(re)installing $(GOBIN)/goimports-v0.0.0-20200925163029-4c77dbd9e533"
+	@cd $(BINGO_DIR) && $(GO) build -mod=mod -modfile=goimports.mod -o=$(GOBIN)/goimports-v0.0.0-20200925163029-4c77dbd9e533 "golang.org/x/tools/cmd/goimports"
+
+GOLANGCI_LINT := $(GOBIN)/golangci-lint-v1.31.0
+$(GOLANGCI_LINT): $(BINGO_DIR)/golangci-lint.mod
+	@# Install binary/ries using Go 1.14+ build command. This is using bwplotka/bingo-controlled, separate go module with pinned dependencies.
+	@echo "(re)installing $(GOBIN)/golangci-lint-v1.31.0"
+	@cd $(BINGO_DIR) && $(GO) build -mod=mod -modfile=golangci-lint.mod -o=$(GOBIN)/golangci-lint-v1.31.0 "github.com/golangci/golangci-lint/cmd/golangci-lint"
+
+MISSPELL := $(GOBIN)/misspell-v0.3.5-0.20180309020325-c0b55c823952
+$(MISSPELL): $(BINGO_DIR)/misspell.mod
+	@# Install binary/ries using Go 1.14+ build command. This is using bwplotka/bingo-controlled, separate go module with pinned dependencies.
+	@echo "(re)installing $(GOBIN)/misspell-v0.3.5-0.20180309020325-c0b55c823952"
+	@cd $(BINGO_DIR) && $(GO) build -mod=mod -modfile=misspell.mod -o=$(GOBIN)/misspell-v0.3.5-0.20180309020325-c0b55c823952 "github.com/client9/misspell/cmd/misspell"
+
