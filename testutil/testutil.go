23ec93ca9505fae0cae48cb5de33312b54607c86
Krasi Georgiev <8903888+krasi-georgiev@users.noreply.github.com>
2022-04-28 12:57:36 +0300

    added testutil helper IsRevertErr without the need for message
    Signed-off-by: Krasi Georgiev <8903888+krasi-georgiev@users.noreply.github.com>

--- testutil/testutil.go
+++ testutil/testutil.go
@@ -60,11 +60,12 @@ func Assert(tb testing.TB, condition bool, v ...interface{}) {
 	tb.Fatalf("\033[31m%s:%d: "+msg+"\033[39m\n\n", filepath.Base(file), line)
 }
 
-func IsRevertErrWithMessage(tb testing.TB, err error, msg string) {
-	if err == nil {
-		tb.Fatalf("\033[31m expected an error, but got nil\033[39m\n")
-	}
+func IsRevertErr(tb testing.TB, err error) {
 	_, file, line, _ := runtime.Caller(1)
+
+	if err == nil {
+		tb.Fatalf("\033[31m%s:%d: expected an error, but got nil\033[39m\n", filepath.Base(file), line)
+	}
 
 	errRpc, ok := errors.Cause(err).(rpc.Error)
 	if !ok {
@@ -76,11 +77,19 @@ func IsRevertErrWithMessage(tb testing.TB, err error, msg string) {
 	if errRpc.ErrorCode() != -32603 {
 		tb.Fatalf("\033[31m%s:%d:not a expected revert code\n\n unexpected error: %s\033[39m\n\n", filepath.Base(file), line, err.Error())
 	}
+}
 
-	if msg != "" {
-		if errRpc.Error() != fmt.Sprintf("Error: VM Exception while processing transaction: reverted with reason string '%v'", msg) {
-			tb.Fatalf("\033[31m%s:%d:not a expected revert message:"+msg+"\n\n unexpected error: %s\033[39m\n\n", filepath.Base(file), line, err.Error())
-		}
+func IsRevertErrWithMessage(tb testing.TB, err error, msg string) {
+	IsRevertErr(tb, err)
+
+	_, file, line, _ := runtime.Caller(1)
+
+	errRpc, ok := errors.Cause(err).(rpc.Error)
+	if !ok {
+		tb.Fatalf("\033[31m%s:%d:not a rpc.Error\n\n unexpected error: %s\033[39m\n\n", filepath.Base(file), line, err.Error())
+	}
+	if errRpc.Error() != fmt.Sprintf("Error: VM Exception while processing transaction: reverted with reason string '%v'", msg) {
+		tb.Fatalf("\033[31m%s:%d:not a expected revert message:"+msg+"\n\n unexpected error: %s\033[39m\n\n", filepath.Base(file), line, err.Error())
 	}
 }
 
