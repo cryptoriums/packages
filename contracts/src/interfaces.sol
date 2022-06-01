e63b81ca6bdc78be1a9b54e5ed5388049e2244f4
Krasi Georgiev <8903888+krasi-georgiev@users.noreply.github.com>
2022-06-01 12:36:19 +0300

    added interface IERC20Metadata
    Signed-off-by: Krasi Georgiev <8903888+krasi-georgiev@users.noreply.github.com>

--- contracts/src/interfaces.sol
+++ contracts/src/interfaces.sol
@@ -88,6 +88,23 @@ interface IERC20 {
     );
 }
 
+interface IERC20Metadata is IERC20 {
+    /**
+     * @dev Returns the name of the token.
+     */
+    function name() external view returns (string memory);
+
+    /**
+     * @dev Returns the symbol of the token.
+     */
+    function symbol() external view returns (string memory);
+
+    /**
+     * @dev Returns the decimals places of the token.
+     */
+    function decimals() external view returns (uint8);
+}
+
 interface Owneable {
     function setOwner(address _newOwner) external;
 }
